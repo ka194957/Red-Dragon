79c46a8fa7f1c90ae49d65e0b14609a59511e308
ka194957 <111072015+ka194957@users.noreply.github.com>
2022-08-31 15:55:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,15 +11,25 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'your key',
 }
 
-global.alivemsg = `📤I am alive now😇
-🤷‍♀️How can I help you?😉` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `📤I am alive now😉❤️ආදරෙයි❤️
+🤷‍♀️How can I help you?😉
+❤️ආදරෙන් සාදරෙන් පිලිගන්නවා❤️
+වට්සැප් පැකේජ් එකෙන්ම සින්දු , 
+වීඩියෝ ඩවුන්ලෝඩ් කරමු.
+තව ගොඩක් දේවල් තියෙනවා
+පහල තියෙන කමාන්ඩ් Use කරන්න
+මුලට ඩොට් එක ( . )අනිවාර්ය නෑ
+.alive = බොට් වැඩද බැලීමට
+.song = සිංදු download කිරීමට
+.video = video download කිරීමට
+.menu = වෙනත් command ` //Costomize Alive Message (type your message in `` )
 
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
+global.available = false //auto available (true to on, false to off)
 
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
 global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
@@ -29,8 +39,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+94716994931'] // owner number
+global.ownername = "❤️𝐑𝐈𝐘𝐀𝐋✪𝐋𝐎𝐊𝐔✪𝐊𝐈𝐍𝐆✪𝐀𝐋𝐏𝐇𝐀❤️(𝕄𝔸𝕃𝕀𝕐𝔸)" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
@@ -44,19 +54,19 @@ global.antibad = "false" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
 global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
-global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
+global.welcom_msg = "false" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['+94716994931']
+global.ownertag = '+94716994931' //your tag number
+global.botname = '𖡹𖡹🇱🇰👿🌈𝐊𝐈𝐍𝐆 🤗𝐀𝐋𝐏𝐇𝐀𖡹𝐁𝐎𝐓🌈👿🇱🇰𖡹𖡹' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "❁༗✮🦅❂⃝𝑫𝑹𝑨𝑮𝑶𝑵⃝༈❥❂" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ ◉ හදවතක් තියෙන කෙල්ලන්ගේ මූණේ ලපේ සුන්දරයි ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94716994931'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
